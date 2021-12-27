8d853a0e9ed166dc3559e6a30f96385814c0030c
Epikem <epikem1@gmail.com>
2021-12-27 07:04:24 +0000

    add test02

---
+++ src/tests/02_periodic_events.ts
@@ -0,0 +1,36 @@
+import { expect } from "chai";
+import { Observable } from "rxjs";
+const ___ = "fill this with correct answer";
+const list: Array<string> = new Array();
+
+const observable = new Observable<string>(subscriber => {
+    setInterval(() => {
+        subscriber.next(___);
+    }, 1000);
+
+    setTimeout(() => {
+        // uncomment below line and change ___ to correct method
+        // subscriber.___(); 
+    }, 5000);
+});
+
+export default function (done: Mocha.Done) {
+    let count = 0;
+    console.log('before subscribe');
+    observable.subscribe({
+        next(item) {
+            console.log('received value:' + item);
+            list.push(item);
+        },
+        error(err) {
+            console.error('err:', err);
+        },
+        complete() {
+            console.log('done');
+            expect(list).to.have.length.greaterThan(3);
+            expect(list).to.include.members(['hi']);
+            done();
+        }
+    });
+    console.log('after subscribe');
+}
