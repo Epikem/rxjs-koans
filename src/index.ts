b681b6fdc9cb41d1f96e7334bb9cd2b1d0b2c620
Epikem <epikem1@gmail.com>
2021-12-29 05:12:30 +0000

    refactor test run - use async dynamic import - use for loop - remove runner variable

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,21 @@
 import fs from 'fs';
 
 import Mocha from 'mocha';
-import Test01 from './tests/01_simple_observables';
-import Test02 from './tests/02_periodic_events';
-import Test03 from './tests/03_cancelling_subscription';
-import Test04 from './tests/04_creation_operators_of';
+import { load_tests } from './tests';
 
-const ___ = "fill this with correct answer";
 const CheckpointPath = './checkpoint';
 
-var Test = Mocha.Test;
 var Suite = Mocha.Suite;
 
 var mocha = new Mocha();
 mocha.timeout('10s');
 var suite: Mocha.Suite;
 var currentLevel = 0;
+var tests: Mocha.Test[] = [];
 
-const Tests = [
-    new Test('Test 01 - simple observables', Test01),
-    new Test('Test 02 - periodic events', Test02),
-    new Test('Test 03 - cancelling subscription', Test03),
-    new Test('Test 04 - creation operators', Test04),
-]
-
-function init() {
+async function init() {
     var isCheckpointExists = fs.existsSync(CheckpointPath);
+    tests = await load_tests();
 
     if (!isCheckpointExists) {
         fs.writeFileSync(CheckpointPath, '0');
@@ -35,71 +25,39 @@ function init() {
     currentLevel = +fs.readFileSync(CheckpointPath, { encoding: 'utf8' });
     suite = Suite.create(mocha.suite, `Question. `);
     
-    suite.addTest(Tests[currentLevel])
+    suite.addTest(tests[currentLevel]);
 
     suite.timeout('10s');
+
+    mocha.run()
+        .on('fail', () => {
+            setTimeout(() => {
+                console.error('test FAILED!');
+            }, 1000);
+        })
+        .on('pass', () => {
+            setTimeout(() => {
+                console.info('congratulations. test PASSED!');
+                console.info('proceeding to next level...');
+            }, 1000);
+            setTimeout(() => {
+                currentLevel = +currentLevel + 1;
+        
+                if (currentLevel === tests.length) {
+                    console.info('congratulations. you have passed all levels!');
+                    return;
+                }
+        
+                console.log('current level: ', currentLevel);
+        
+                suite.tests = [];
+        
+                suite.addTest(tests[currentLevel]);
+        
+                fs.writeFileSync(CheckpointPath, ''+currentLevel, { encoding: 'utf-8' });
+            }, 5000);
+        })
+        
 }
 
 init();
-
-var runner = mocha.run();
-
-runner.on('fail', () => {
-    setTimeout(() => {
-        console.error('test FAILED!');
-    }, 1000);
-})
-
-runner.on('pass', () => {
-    setTimeout(() => {
-        console.info('congratulations. test PASSED!');
-        console.info('proceeding to next level...');
-    }, 1000);
-    setTimeout(() => {
-        currentLevel = +currentLevel + 1;
-
-        if (currentLevel === Tests.length) {
-            console.info('congratulations. you have passed all levels!');
-            return;
-        }
-
-        console.log('current level: ', currentLevel);
-
-        suite.tests = [];
-
-        suite.addTest(Tests[currentLevel]);
-
-        fs.writeFileSync(CheckpointPath, ''+currentLevel, { encoding: 'utf-8' });
-    }, 5000);
-})
-
-
-
-// describe('index.ts 테스트', () => {
-//     it('should pass', () => {
-//         expect('asd', 'what').to.be.equal('asd')
-//     })
-// });
-
-// describe('purity', () => {
-//     it('is pure', () => {
-
-//         const observable = new Observable(subscriber => {
-//             subscriber.next(1);
-//             subscriber.next(2);
-//             subscriber.next(3);
-//             setTimeout(() => {
-//                 subscriber.next(4);
-//                 subscriber.complete();
-//             }, 1000);
-//         });
-
-//         console.log('just before subscribe');
-//         observable.subscribe({
-//             next(x) { console.log('got value ' + x); },
-//             error(err) { console.error('something wrong occurred: ' + err); },
-//             complete() { console.log('done'); }
-//         });
-//         console.log('just after subscribe');
-//     })
-// })
---
+++ src/tests/index.ts
@@ -0,0 +1,20 @@
+import Mocha, { Test } from 'mocha';
+
+const Paths = {
+    Test01: '01_simple_observables',
+    Test02: '02_periodic_events',
+    Test03: '03_cancelling_subscription',
+    Test04: '04_creation_operators_of',
+}
+
+export async function load_tests () {
+    const Tests: Mocha.Test[] = [];
+
+    for (let [key, value] of Object.entries(Paths)) {
+        console.log('loading test', value);
+        const test = new Test('title', await import(`./${value}`));
+        Tests.push(test);
+    }
+
+    return Tests;
+}
